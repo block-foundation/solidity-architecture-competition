4fb4d366ed8330cbbcbd93276019378f7b3173d5
Lars van Vianen <lars@vianen.com>
2023-08-05 08:35:13 +0200

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -17,38 +17,57 @@
 
 
 // Importing the ethers object from hardhat
-import { ethers } from 'hardhat'
+import { ethers } from "hardhat"
+
 
 /**
- * @function main
- * @dev The main function of the deployment script. It deploys the contract and logs the account
- *      used for deployment and the address of the deployed contract.
+ *  @function main
+ *  @dev The main function of the deployment script. It deploys the contract 
+ *  and logs the account  used for deployment and the address of the
+ *  deployed contract.
  */
 async function main() {
+
     // Get the account used for deployment
     const [deployer] = await ethers.getSigners();
 
-    console.log("Deploying the contracts with the account:", deployer.address);
+    console.log(
+        "Deploying the contracts with the account:",
+        deployer.address
+    );
 
     // Get and log the balance of the deployer account
-    console.log("Account balance:", (await deployer.getBalance()).toString());
+    console.log(
+        "Account balance:",
+        (await deployer.getBalance()).toString()
+    );
 
     // Get the contract factory for the ArchitecturalCompetition contract
-    const ArchitecturalCompetition = await ethers.getContractFactory("ArchitecturalCompetition");
+    const ArchitecturalCompetition = await ethers.getContractFactory(
+        "ArchitecturalCompetition"
+    );
     
-    // Deploy the contract with an entry fee of 1 ether and a voting duration of 7 days
-    const competition = await ArchitecturalCompetition.deploy(ethers.utils.parseEther('1'), 7);
+    // Deploy the contract with an entry fee of 1 ether and a voting 
+    // duration of 7 days
+    const competition = await ArchitecturalCompetition.deploy(
+        ethers.utils.parseEther("1"),
+        7
+    );
 
     // Wait for the contract to be deployed
     await competition.deployed();
 
     // Log the address of the deployed contract
-    console.log("ArchitecturalCompetition contract deployed to:", competition.address);
+    console.log(
+        "ArchitecturalCompetition contract deployed to:",
+        competition.address
+    );
 }
 
 /**
- * @function main
- * @dev Entry point of the script. Calls the main function and handles errors and exit codes.
+ *  @function main
+ *  @dev Entry point of the script. Calls the main function and handles errors
+ *  and exit codes.
  */
 main()
     .then(() => process.exit(0))
